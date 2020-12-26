47e64eb20100d4e2384e59427b7a7803959d2170
Stephan Soller <stephan.soller@helionweb.de>
2020-12-26 20:09:25 +0100

    Fixed the line output of php2-all-strpos.php.

--- php2-all-strpos.php
+++ php2-all-strpos.php
@@ -6,12 +6,12 @@ $file = $argv[2];
 $data = file_get_contents($file);
 $pos = 0;
 while ( ($pos = strpos($data, $search, $pos)) !== false ) {
-    $before = substr($data, 0, $pos);
-    $start_pos = strrpos($before, "\n");
+    $start_pos = strrpos(substr($data, 0, $pos), "\n");
+    $start_pos = ($start_pos === false) ? 0 : $start_pos + 1;
     $end_pos = strpos($data, "\n", $pos);
     if ($end_pos === false)
         $end_pos = strlen($data);
-    echo(substr($data, $start_pos, $end_pos - $start_pos));
+    echo(substr($data, $start_pos, $end_pos - $start_pos) . "\n");
     
     $pos += strlen($search);
 }
